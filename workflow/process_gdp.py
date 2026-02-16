cd9ed868b7f9cc8f3df5fd5805eacf84b30f30ad
Narrara | Nahara <nmelomoura@binghamton.edu>
2026-02-16 18:06:28 -0500

    remove print line (not needed here)

--- workflow/process_gdp.py
+++ workflow/process_gdp.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 # Load and tidy
 data_table = pd.read_csv('./data/raw/gdp-data.csv')
-print(data_table.head())
 
 # Use pd.melt(), pd.pivot_table(), or custom code
 tidy_data_table = data_table.melt(id_vars=['geo', 'name'], var_name='year', value_name='gdp_capita')
